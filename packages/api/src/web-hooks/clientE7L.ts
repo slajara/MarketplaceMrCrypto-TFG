d1702b138b16bb737d9df77f4e4610ad4aaf6512
Samuel Lajara <lajara.agullo@gmail.com>
2024-04-22 19:03:38 +0200

    Prepare to deploy

--- packages/api/src/web-hooks/clientE7L.ts
+++ packages/api/src/web-hooks/clientE7L.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  //uri: "https://mrcryptoindexer-production.up.railway.app/api/",
-  uri: "http://localhost:4000/",
+  //uri: "https://mrcryptoindexer-production.up.railway.app/api/", ANTES
+ uri: "https://indexer.mrcryptonft.com/api/",
+ // uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
